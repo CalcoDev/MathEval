9c993cdc887864bea8c5876624354e8b34053186
Calco Dev <calcopoddev@gmail.com>
2023-10-31 21:39:54 +0200

    Better lexing, but still bad.

--- src/str8.c
+++ src/str8.c
@@ -11,7 +11,7 @@ b8 str8_starts_with(str8 s, str8 cmp)
   if (cmp.length > s.length)
     return 0;
 
-  for (i32 i = 0; i < s.length; ++i)
+  for (i32 i = 0; i < cmp.length; ++i)
   {
     if (s.buffer[i] != cmp.buffer[i])
       return 0;
